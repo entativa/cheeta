4f4d7df954f430e47c03560b6a7b9a87593204a5
entativa <entativa22@gmail.com>
2025-11-22 14:15:57 +0200

    Update settings.gradle.kts

---
+++ cheeta-webapp/settings.gradle.kts
@@ -0,0 +1,9 @@
+rootProject.name = "cheeta-webapp"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        maven { url = uri("https://maven.vaadin.com/vaadin-prereleases") }
+    }
+}
